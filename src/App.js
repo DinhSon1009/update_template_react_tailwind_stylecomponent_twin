d80a728733ba1fb3600cabed63d30b0a324030fe
dinh son <dsmama96@gmail.com>
2022-05-18 23:43:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyles } from "twin.macro";
+import "./App.css";
+import ButtonComponent from "./components";
+import { StyledApp } from "./styles/StyledApp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <ButtonComponent />
+      </StyledApp>
+    </>
   );
 }
 
